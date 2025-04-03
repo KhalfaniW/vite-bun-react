235f98e654e88bb86e78f6777054dca6c4bb4550
Khalfani <khalfaniw@hotmail.com>
2025-04-02 23:24:07 -0500

    Initial commit: Set up Jest testing with React Testing Library

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+};
